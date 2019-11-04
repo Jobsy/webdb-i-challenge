b4270b13f0e17836ad98125d7c1659814b35a3ec
Oluwajoba <oluwajobabello@gmail.com>
2019-11-04 16:01:39 +0100

    test account-router is working from postman / account-router.js

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -3,3 +3,10 @@ const express = require("express");
 const dB = require("../data/dbConfig");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.status(200).json("Hello World")
+})
+
+
+module.exports = router;
