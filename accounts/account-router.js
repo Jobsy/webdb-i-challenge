c799e21c9b2e8a2f11dc0683ec0cd5d70c930e91
Oluwajoba <oluwajobabello@gmail.com>
2019-11-04 20:57:16 +0100

    add router.get end points / account-router.js

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -1,11 +1,27 @@
 const express = require("express");
 
-const dB = require("../data/dbConfig");
+const dB = require("../data/dbConfig.js");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).json("Hello World")
+    // res.status(200).json("Hello World")
+    dB.select("*")
+        .from("accounts")
+        .then((accounts) => {
+            res.status(200).json(accounts)
+        })
+        .catch((err) => {
+            res.status(500).json("errMessage: " + err)
+        })
+    // dB('accounts') // returns promise!!!
+    // .then(result => {
+    //     res.status(200).json(result)
+
+    // })
+    // .catch(error => {
+    //   res.status(500).json({ message: 'this went wrong: ' + error.message });
+    // });
 })
 
 
