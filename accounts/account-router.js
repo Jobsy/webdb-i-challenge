df8631ecfd6381dbadba1492085cd432f90fa4e0
Oluwajoba <oluwajobabello@gmail.com>
2019-11-04 22:22:53 +0100

    add router.get end points / get by id / account-router.js

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -5,23 +5,29 @@ const dB = require("../data/dbConfig.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    // res.status(200).json("Hello World")
-    dB.select("*")
-        .from("accounts")
-        .then((accounts) => {
-            res.status(200).json(accounts)
-        })
-        .catch((err) => {
-            res.status(500).json("errMessage: " + err)
-        })
-    // dB('accounts') // returns promise!!!
-    // .then(result => {
-    //     res.status(200).json(result)
+  // res.status(200).json("Hello World")
+  dB.select("*")
+    .from("accounts")
+    .then((accounts) => {
+      res.status(200).json(accounts)
+    })
+    .catch((err) => {
+      res.status(500).json("errMessage: " + err)
+    })
+})
 
-    // })
-    // .catch(error => {
-    //   res.status(500).json({ message: 'this went wrong: ' + error.message });
-    // });
+router.get("/:id", (req, res) => {
+  // res.status(200).json("Hello World")
+  dB.select("*")
+    .from("accounts")
+    .where("id", "=", req.params.id)
+    .first()
+    .then((accounts) => {
+      res.status(200).json(accounts)
+    })
+    .catch((err) => {
+      res.status(500).json("errMessage: " + err)
+    })
 })
 
 
