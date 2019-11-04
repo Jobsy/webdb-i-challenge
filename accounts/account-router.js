0e9d0de872d8f9114821d6950ff2e818e77a4d94
Oluwajoba <oluwajobabello@gmail.com>
2019-11-04 22:49:08 +0100

    add router.post end points / account-router.js

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -17,11 +17,22 @@ router.get("/", (req, res) => {
 })
 
 router.get("/:id", (req, res) => {
-  // res.status(200).json("Hello World")
   dB.select("*")
     .from("accounts")
     .where("id", "=", req.params.id)
     .first()
+    .then((accounts) => {
+      res.status(200).json(accounts)
+    })
+    .catch((err) => {
+      res.status(500).json("errMessage: " + err)
+    })
+})
+
+router.post("/", (req, res) => {
+  const accountsId = req.body;
+  dB("accounts")
+    .insert(accountsId, "id")
     .then((accounts) => {
       res.status(200).json(accounts)
     })
